23b243ce4bf9ff705ae4dc86d8855595439e5165
cheick011 <cheickbb01@gmail.com>
2025-11-21 11:54:18 +0100

    fichiers

--- data/Base_de_données_ main_courantes.sql
+++ data/Base_de_données_ main_courantes.sql
@@ -1,28 +1,24 @@
-CREATE DATABASE spelo_app ;
-USE spelo_app;
-
-
+CREATE DATABASE spelo_app;
+\c spelo_app;
 
 CREATE TABLE utilisateurs (
     id SERIAL PRIMARY KEY,
     nom_utilisateur VARCHAR(50) UNIQUE NOT NULL,
     mot_de_passe TEXT NOT NULL,
+    role TEXT DEFAULT 'lecteur' CHECK (role IN ('lecteur', 'gestionnaire', 'admin')),
     date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    role TEXT DEFAULT 'lecteur' CHECK (statut IN ('lecteur', 'admin', 'gestionnaire')) 
 );
 
-
-insert INTO TABLE utilisateurs (nom_utilisateur, mot_de_passe, role)  values
-('admin', 'admin','admin'),
+INSERT INTO utilisateurs (nom_utilisateur, mot_de_passe, role) VALUES
+('admin', 'admin', 'admin'),
 ('gestionnaire', 'gestionnaire', 'gestionnaire'),
 ('lecteur', 'lecteur', 'lecteur');
-
 
 CREATE TABLE donnees (
     id SERIAL PRIMARY KEY,
     heure TIME DEFAULT CURRENT_TIME,
-    de TEXT;
-    a TEX
+    de TEXT,
+    a TEXT,
     descriptif TEXT NOT NULL,
     date DATE DEFAULT CURRENT_DATE,
     id_utilisateur INTEGER REFERENCES utilisateurs(id) ON DELETE SET NULL
