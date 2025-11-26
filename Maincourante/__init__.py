255fa67435d076a96d8f85781f10fff82d88794d
cheick011 <cheickbb01@gmail.com>
2025-11-26 13:00:57 +0100

    init

---
+++ Maincourante/__init__.py
@@ -0,0 +1,15 @@
+
+"""
+Projet : Main courante
+Ce projet contient plusieurs modules permettant de modifier, d'ajouter, de supprimer les information ou utilisateurs et envoyer à tous le monde.
+.. moduleauthor:: N'DIAYE Cheick Bounama Boubacar <cheick.n.diaye@etu.univ-poitiers.fr>
+.. moduleauthor:: Gatlin ALLOHO <gatlin.alloho@etu.univ-poitiers.fr>
+.. moduleauthor:: Marème Mboup <mareme.mboup@etu.univ-poitiers.fr>
+.. moduleauthor:: Adja Diarietou Mbengue <adja.diarietou.mbengue@etu.univ-poitiers.fr>
+"""
+
+# Import des modules du projet
+import login
+import admin
+import gestion
+import client
