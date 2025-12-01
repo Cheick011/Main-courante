039ad4899b49c520bb0fd80257aac95669d546e5
cheick011 <cheickbb01@gmail.com>
2025-12-01 13:32:53 +0100

    test

---
+++ test/login.py
@@ -0,0 +1,58 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QApplication
+
+class AdminPage(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Page Admin")
+        self.setGeometry(300, 300, 400, 200)
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel("Bienvenue Admin !"))
+        self.setLayout(layout)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = AdminPage()
+    window.show()
+    sys.exit(app.exec_())
+    
+    
+    
+    import sys
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QApplication
+
+class ClientPage(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Page Client")
+        self.setGeometry(300, 300, 400, 200)
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel("Bienvenue Client !"))
+        self.setLayout(layout)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = ClientPage()
+    window.show()
+    sys.exit(app.exec_())
+
+
+import sys
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QApplication
+
+class GestionPage(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Page Gestionnaire")
+        self.setGeometry(300, 300, 400, 200)
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel("Bienvenue Gestionnaire !"))
+        self.setLayout(layout)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = GestionPage()
+    window.show()
+    sys.exit(app.exec_())
