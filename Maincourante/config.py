083a41d43e32de72bd55ca0449794c26c9f5932e
Cheick011 <cheick.n.diaye@etu.univ-poitiers.fr>
2025-12-31 16:12:27 +0100

    ajout des information

--- Maincourante/config.py
+++ Maincourante/config.py
@@ -1,2 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: config
+   :platform: Unix, windows
+   :synopsis: module pour definir le mutlicast
+
+.. moduleauthor:: N'DIAYE Cheick Bounama Boubacar <cheick.n.diaye@etu.univ-poitier.fr>
+
+
+"""
+
 MULTICAST_GRP = "224.1.1.1"
 PORT = 5007
