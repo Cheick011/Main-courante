1dc811c5e4da2faa67f4c7d3c09e8484b0260a51
Cheick011 <cheick.n.diaye@etu.univ-poitiers.fr>
2025-12-31 16:08:51 +0100

    nom de module

--- Maincourante/Connexion_dataBase.py
+++ Maincourante/Connexion_dataBase.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: Connexion_dataBase
+   :platform: Unix, windows
+   :synopsis: module pour connecter l'application à la base de donnée postgresql
+
+.. moduleauthor:: N'DIAYE Cheick Bounama Boubacar <cheick.n.diaye@etu.univ-poitier.fr>
+
+
+"""
+
 import psycopg2
 
 def connexion():
