02712b96a8373345df999af499429f5352dce93a
Mareme23 <mboupmareme23e@icloud.com>
2026-01-05 10:11:24 +0100

    Add files via upload

---
+++ Maincourante/utilisateurs.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  4 22:53:32 2026
+
+@author: etudiant
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QTextEdit,QMenuBar,QMenu,QToolBar,QWidget, QAction, QMessageBox, QLineEdit,QFormLayout,QVBoxLayout,QHBoxLayout, QPushButton, QGroupBox, QTableWidget, QTableWidgetItem, QSizePolicy
+from PyQt5.QtGui import QIcon, QKeySequence
+
+class GestionPage(QMainWindow):
+    TITRE_FENETRE = "Page spéléologues"
+
+    def __init__(self): 
+            super().__init__()
+    
+            # paramétrage de la fenêtre
+            self.setWindowTitle(GestionPage.TITRE_FENETRE)
+            self.resize(900, 600)
+            
+            self.__menuBar = QMenuBar()
+            self.setMenuBar(self.__menuBar)
+            
+            self.__help = self.__menuBar.addMenu('&Apropos')
+            
+            self.__action_apropos = QAction(QIcon('actions/stock_search.png'), 'A propos', self)
+            #self.__action_apropos.setStatusTisp("A propos")
+            
+            self.__help.addAction(self.__action_apropos)
+            
+            self.__action_apropos.triggered.connect(self.a_propos)
+            
+            #creation  bloc général
+            self.__bloc_général = QWidget() 
+            self.__bloc_général_lay = QVBoxLayout()
+            self.__bloc_général.setLayout(self.__bloc_général_lay)
+            self.setCentralWidget(self.__bloc_général)
+            
+            #creation bloc haut
+            
+            self.__bloc_haut = QGroupBox()
+            self.__bloc_haut_lay = QHBoxLayout()
+            self.__bloc_haut.setLayout(self.__bloc_haut_lay)    
+            self.__bloc_général_lay.addWidget(self.__bloc_haut) 
+           
+            
+            self.__titre= QLabel("Application spéléo-sauvetage")
+            self.__spéléologues=QLabel("Page spéléologues")
+            self.__Déconnecter=QPushButton("Déconnecter")
+            
+          
+            
+            self.__bloc_haut_lay.addWidget(self.__titre)
+            self.__bloc_haut_lay.addWidget(self.__spéléologues)
+            self.__bloc_haut_lay.addWidget(self.__Déconnecter)
+            
+            # création d’un bloc pour centrer le tableau
+            self.__bloc_tableau_conteneur = QWidget()
+            self.__bloc_tableau_conteneur_lay = QHBoxLayout()
+            self.__bloc_tableau_conteneur.setLayout(self.__bloc_tableau_conteneur_lay)
+    
+            self.__bloc_général_lay.addWidget(self.__bloc_tableau_conteneur)
+             
+    
+            # le tableau
+            self.__bloc_tableau = QTableWidget() #TableWidget n’est pas censé recevoir un layout, ce n’est pas un conteneur.
+           
+           
+            self.__bloc_tableau.setRowCount(5)
+            self.__bloc_tableau.setColumnCount(4)
+            self.__bloc_tableau.setHorizontalHeaderLabels(["Heure", "De", "À", "Description"])
+            self.__bloc_tableau.verticalHeader().setVisible(False)
+            self.__bloc_tableau.setFixedSize(600,380)
+            self.__bloc_tableau.setEditTriggers( QTableWidget.NoEditTriggers)
+            
+            # stretch permet de placer le tableu au centre, on ajoute le tableau au layout du conteneur
+            self.__bloc_tableau_conteneur_lay.addStretch()
+            self.__bloc_tableau_conteneur_lay.addWidget(self.__bloc_tableau)
+            self.__bloc_tableau_conteneur_lay.addStretch()
+    
+    def a_propos(self):
+                
+         
+            QMessageBox.information(self,'A propos','Cette application a été développé par un groupe de 4 étudiants en BUT2 FI RT promotion 2025-2026 dans le cadre de leur SAÉ "Développer des applications communicantes"')
+    def deconnecter(self):
+        rep = QMessageBox.question(
+            self,
+            "Déconnexion",
+            "Voulez-vous vous déconnecter ?",
+            QMessageBox.Yes | QMessageBox.No
+        )
+        if rep == QMessageBox.Yes:
+            self.close()
+def main():
+            application = QApplication(sys.argv)
+            window = GestionPage()
+            window.show()
+            sys.exit(application.exec_())
+        
+if __name__ == '__main__':
+            main()
+        
