a81148bcfa5bc2f174b42c80697866aa26efec4f
Cheick011 <cheick.n.diaye@etu.univ-poitiers.fr>
2026-01-11 01:40:22 +0100

    test

--- test/notification.py
+++ test/notification.py
@@ -1,15 +1,1 @@
-# test_synchro.py
-from synchro import synchro
 
-data = {
-    "utilisateurs": [
-        {"id": 1, "nom_utilisateur": "user1", "mot_de_passe": "123", "role": "admin", "date_creation": "2026-01-10"}
-    ],
-    "donnees": [
-        {"id": 1, "date": "2026-01-10", "heure": "12:00", "de": "PC", "a": "Cheick", "descriptif": "test", "id_utilisateur": 1}
-    ]
-}
-
-s = synchro()
-s.synchro_Complete(data)
-print("Test synchro complet terminé")
