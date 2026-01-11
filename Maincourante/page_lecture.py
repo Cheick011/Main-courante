5392daeaeae446f727475f5bea0f16a7b5558c29
diarry18 <mbengueadja05@gmail.com>
2026-01-11 18:57:16 +0100

    Update page_lecture.py

--- Maincourante/page_lecture.py
+++ Maincourante/page_lecture.py
@@ -61,7 +61,7 @@ class ClientPage(QMainWindow):
         
         
         # ===== ACTIONS =====
-        self.__action_apropos = QAction(QIcon('actions/logo-a-propos.png'),'A propos', self)
+        self.__action_apropos = QAction(QIcon('actions/icone_3.png'),'A propos', self)
 
         
         self.__help.addAction(self.__action_apropos)
